00d20635b113730ddc8cbb22453438de6d20b9a8
65850 <65850@VSTTFamily>
2023-11-25 01:16:07 +0800

    Function 16 . Add Like to like entity table Add entry in notifcationlog table

--- xyzcar/src/main/java/com/educlaas/xyzcar/service/NotificationLogService.java
+++ xyzcar/src/main/java/com/educlaas/xyzcar/service/NotificationLogService.java
@@ -1,23 +1,28 @@
 package com.educlaas.xyzcar.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educlaas.xyzcar.entity.LikeEntity;
 import com.educlaas.xyzcar.entity.NotificationLog;
+import com.educlaas.xyzcar.entity.Post;
+import com.educlaas.xyzcar.entity.User;
 import com.educlaas.xyzcar.repository.NotificationLogRepository;
+import com.educlaas.xyzcar.repository.PostRepository;
 
 @Service
 public class NotificationLogService {
 
     @Autowired
     private NotificationLogRepository notificationLogRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-    public List<NotificationLog> getAllNotificationLogs() {
-        return notificationLogRepository.findAll();
-    }
+   
 
     public Optional<NotificationLog> getNotificationLogById(Long id) {
         return notificationLogRepository.findById(id);
@@ -37,11 +42,34 @@ public class NotificationLogService {
     
     
     //Function 21 
-    public void createNotificationLog(Long userId, Long postId) {
+    public void createNotificationLog(Long userId, Long postId,Integer targetUserId) {
     	
     }
     
+    //Function16 Add likes to NotificationLog
+    @Autowired
+    private NotificationLogRepository notificationLogRepository1;
+
+    public NotificationLog addLikesToNL(User user, Post post, Integer targetUserId, int notificationType) {
+        NotificationLog notificationLog = new NotificationLog();
+        notificationLog.setCreatedDate(new Date());
+        notificationLog.setUser(user);
+        notificationLog.setTargetUserID(targetUserId);
+        
+        // Set notification type based on the action (like = 1, dislike = 2)
+        if (notificationType == 1 || notificationType == 2) {
+            notificationLog.setNotificationType(notificationType);
+        } else {
+            throw new IllegalArgumentException("Invalid notification type");
+        }
+
+        // Set other attributes in the notification log as needed
+        
+        NotificationLog addedNotificationLog = notificationLogRepository.save(notificationLog);
+
+        return addedNotificationLog;
     
-    
-    
+    }
 }
+    
+
