2095bcd8d1d0d9063fe3f833d46b29e94956206f
Izack Chee <izackchee@gmail.com>
2023-11-27 20:01:05 +0800

    follow functions

--- xyzcar/src/main/java/com/educlaas/xyzcar/repository/FollowRepository.java
+++ xyzcar/src/main/java/com/educlaas/xyzcar/repository/FollowRepository.java
@@ -1,9 +1,15 @@
 package com.educlaas.xyzcar.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educlaas.xyzcar.entity.Follow;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+	List<Follow> findByUserId(Long userId);
     // You can add custom query methods here if needed
+
+	List<Follow> findByUserIdAndStatus(Long userId, int status);
 }
--- xyzcar/src/main/java/com/educlaas/xyzcar/repository/UserRepository.java
+++ xyzcar/src/main/java/com/educlaas/xyzcar/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.educlaas.xyzcar.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educlaas.xyzcar.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+	static List<User> findByUsernameContainingIgnoreCase(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
 
