ce93b7a074ec0be89874ed8dbedf56286359ccac
heartsmith92 <mdfirdausthen92@gmail.com>
2023-12-06 21:53:09 +0800

    Updated NotificationLog Filter Function to add postid

--- xyzcar/src/main/java/com/educlaas/xyzcar/repository/NotificationLogRepository.java
+++ xyzcar/src/main/java/com/educlaas/xyzcar/repository/NotificationLogRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.educlaas.xyzcar.entity.LikeEntity;
 import com.educlaas.xyzcar.entity.NotificationLog;
@@ -14,5 +16,7 @@ public interface NotificationLogRepository extends JpaRepository<NotificationLog
 	LikeEntity save(LikeEntity like);
 	
     List<NotificationLog> findByUser_Id(Long userId);
-	
+    
+    @Query("SELECT nl FROM NotificationLog nl WHERE nl.user.id = :userId AND nl.postid = :postId")
+    List<NotificationLog> findByUser_IdAndPostid(@Param("userId") Long userId, @Param("postId") Long postId);
 }
